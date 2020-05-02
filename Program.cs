089af206ef8f3d6dff7607900ce9d8a5f1093663
Phương Anh Như <nhuphuonganh001@gmail.com>
2020-05-02 13:39:10 +0200

    Add required word mixing method

--- Program.cs
+++ Program.cs
@@ -6,7 +6,25 @@ namespace wordTwister
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           Console.Write("Geben Sie das Wort ein: "); 
+            string str = Console.ReadLine(); 
+            Console.WriteLine(wordMixing(str)); 
+        }
+
+        static string wordMixing(string startingWord) {
+            Random random = new Random();
+
+            char[] startingArray = startingWord.ToCharArray(0, startingWord.Length); 
+            for (int i = 1; i < startingArray.Length - 1; i++) {                     
+                for (int j = i + 1; j < startingArray.Length - 1; j++) {
+                    if (random.Next(10) <= random.Next(10)) {                        
+                        char temp = startingArray[i];
+                        startingArray[i] = startingArray[j];
+                        startingArray[j] = temp;
+                    }
+                }
+            }
+            return new string(startingArray);
         }
     }
 }
